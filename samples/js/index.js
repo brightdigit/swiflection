8d49f7c2966d293673b7d77f23d4affe42b90b7e
Leo Dion <leogdion@brightdigit.com>
2014-12-10 14:22:02 -0500

    working on js code

--- samples/js/index.js
+++ samples/js/index.js
@@ -1,1 +1,17 @@
-index.js
+myprotocol = function () {
+
+};
+
+myclass = new myprotocol ();
+
+myclass.prototype = {
+  helloWorld : function () {
+    console.log("hello world");
+  }
+};
+
+var variables = global || window || this;
+
+for (var name in variables) {
+  console.log(name + ": " + global.hasOwnProperty(name));
+}
