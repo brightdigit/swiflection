9710bea9730e49ff3c8617de985712705ff4d84d
Leo Dion <leogdion@brightdigit.com>
2017-06-06 12:55:24 -0400

    migrating to xcode9

---
+++ Swiflection-macOS/Swiflection_macOS.h
@@ -0,0 +1,18 @@
+//
+//  Swiflection_macOS.h
+//  Swiflection-macOS
+//
+//  Created by Leo Dion on 6/6/17.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Swiflection_macOS.
+FOUNDATION_EXPORT double Swiflection_macOSVersionNumber;
+
+//! Project version string for Swiflection_macOS.
+FOUNDATION_EXPORT const unsigned char Swiflection_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swiflection_macOS/PublicHeader.h>
+
+
